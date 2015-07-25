3890c2dd6930fd8c0b3f2ed20b20f26a4ea57a85
MuritalaA <sulymana@j-sas.com>
2015-07-25 12:44:55 -0400

    commit 2

---
+++ angularjs_tutorial-master/bases/base_tutorial/base_tutorial/app/app.js
@@ -0,0 +1,13 @@
+﻿(function () {
+
+    'use strict';
+
+    var moduleId = 'app';
+
+    angular.module(moduleId, []);
+
+    angular.element(document).ready(function () {
+        angular.element(document.body).attr('ng-controller', 'shellController as shellCtrl');
+        angular.bootstrap(document, [moduleId]);
+    });
+})();
---
+++ angularjs_tutorial-master/bases/base_tutorial/base_tutorial/app/controllers/shell-controller.js
@@ -0,0 +1,17 @@
+﻿(function () {
+
+    'use strict';
+
+    var moduleId = 'app';
+    var controllerId = 'shellController';
+
+    angular.module(moduleId).controller(controllerId, shellController);
+
+    shellController.$inject = [];
+
+    function shellController() {
+        var vm = this;
+        vm.name = 'Base Tutorial';
+    }
+
+})();
